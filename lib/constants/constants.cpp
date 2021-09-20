8b08d98e54504415ce191039ce0987d3f8d32cb4
jbs <johnbspiegel@gmail.com>
2021-09-20 11:17:24 -0700

    Bump version to v0.0.3

--- lib/constants/constants.cpp
+++ lib/constants/constants.cpp
@@ -1,6 +1,6 @@
 #include "constants.h"
 
-const char* kPneumaticVersion = "v0.0.2";
+const char* kPneumaticVersion = "v0.0.3";
 const char* kReleasesUrl = "https://dev.jspiegel.net/pneumatic/releases.json";
 const char* kDeviceConfig = "tdisplay";
 
